9c3985a7edb8b936611ec5a2d8d0ea8b160fc341
Samia <samiaab1990@gmail.com>
2022-11-10 13:23:00 -0500

    slight changes to space(font size, etc.)

--- space/space.R
+++ space/space.R
@@ -6,7 +6,8 @@ library(sf)
 library(stringr)
 library(ggfx)
 library(shadowtext)
-library(geomtextpath)
+library(ggrepel)
+
 
 # title font
 #sysfonts::font_add_google("Archivo","Archivo", bold.wt=700)
@@ -41,13 +42,13 @@ shp_file<-dir %>%
 japan_shp<-st_read(shp_file)
 
 # get raster 
-files<-list.files(dir, full.name=TRUE) %>%
+file<-list.files(dir, full.name=TRUE) %>%
 str_subset('.tif$')
 
-nightlights_raster<-raster(files[3])
+nightlights_raster<-raster(file)
 
 # match shapefile and raster coordinate system
-japan_shp<-st_transform(seasia_shp1,st_crs(nightlights_raster))
+japan_shp<-st_transform(japan_shp,st_crs(nightlights_raster))
 
 ## crop raster to shapefile extent
 ## mask the cropped raster to the geographic boundaries
@@ -87,9 +88,9 @@ geom_text_repel(data = jpn_cities %>% filter(city %in% c("Sendai","Saitama","Kaw
 geom_text_repel(data = jpn_cities %>% filter(city %in% c("Yokohama")), aes(x=lng , y=lat, label=city), color="#9C9C9C", nudge_x= .1*x_diff, nudge_y=-.08*y_diff,  segment.linetype=5, segment.size=.1,family="Advent Pro", size=3.5, segment.curvature = -1e-20)+
 geom_text_repel(data = jpn_cities %>% filter(city %in% c("Kōbe","Ōsaka")), aes(x=lng , y=lat, label=city), color="#9C9C9C", nudge_x= .05*x_diff, nudge_y=-.08*y_diff,  segment.linetype=5, segment.size=.1,family="Advent Pro", size=3.5, segment.curvature = -1e-20)+
 geom_shadowtext(aes(x=jpn_bbox[1]+.03*x_diff,y=jpn_bbox[4]-.1*y_diff,label=title), size=10, family="Orbitron", color="#0E0E0E", bg.color= "#9C9C9C", alpha=.7)+
-geom_text(aes(x=jpn_bbox[1]+.03*x_diff,y=jpn_bbox[4]-.18*y_diff, label=subtitle), family="Advent Pro", color="#9C9C9C")+
-geom_segment(aes(x=jpn_bbox[1]-.15*x_diff, y=jpn_bbox[4]-.28*y_diff, xend=jpn_bbox[1]+.27*x_diff,yend=jpn_bbox[4]-.28*y_diff), linetype="dotted", size=.3, color="#9C9C9C", arrow=arrow(type="closed",length=unit(.015,"npc")))+
-geom_text(aes(x=jpn_bbox[1]+.04*x_diff,y=jpn_bbox[4]-.29*y_diff,label="Increasing light emission"),family="Advent Pro", color="#9C9C9C",size=2.5)+
+geom_text(aes(x=jpn_bbox[1]+.03*x_diff,y=jpn_bbox[4]-.18*y_diff, label=subtitle), family="Advent Pro", color="#9C9C9C", size=5)+
+geom_segment(aes(x=jpn_bbox[1]-.15*x_diff, y=jpn_bbox[4]-.28*y_diff, xend=jpn_bbox[1]+.27*x_diff,yend=jpn_bbox[4]-.28*y_diff), linetype="dashed", size=.3, color="#9C9C9C", arrow=arrow(type="closed",length=unit(.013,"npc")))+
+geom_text(aes(x=jpn_bbox[1]+.04*x_diff,y=jpn_bbox[4]-.29*y_diff,label="Increasing light emission"),family="Advent Pro", color="#9C9C9C",size=3.5)+
 geom_textbox(aes(x=jpn_bbox[1]-.17*x_diff,y=jpn_bbox[2]+.02*y_diff,label="<b><span style='font-family:\"Font Awesome 5 Free Solid\"'>&#xf279;</span><span style='font-family:\"Advent Pro\"'> #30DayMapChallenge </span></b><br><b><span style='font-family:\"Advent Pro\"'>Source:</b> Earth Observation Group (lights) and simplemaps (cities)<br><b>Map made by:</b> Samia B</span> <span style='font-family: \"Font Awesome 5 Brands Regular\"'>&#xf09b;</span><span style='font-family:\"Advent Pro\"'> <b>samiaab1990</b></span>"), color="#9C9C9C",size=2, fill=NA, box.colour=NA, width=unit(.28,"npc"))+
 theme(plot.margin = margin(l=50, unit="mm"),
       legend.position = c(.05,.75),
@@ -105,4 +106,4 @@ coord_sf(clip="off",
          xlim = c(jpn_bbox[1],150),
          expand=FALSE)
 
-ggsave(plot = p, filename = "space.png", width=8, height=6, units="in", dpi=300, device=ragg::agg_png, bg="#0E0E0E")
+ggsave(plot = p, filename = "space_test.png", width=8, height=6, units="in", dpi=300, device=ragg::agg_png, bg="#0E0E0E")
